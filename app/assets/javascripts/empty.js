efb0ebbc95676d5153f45a2c8c68bb3e3e6c80f5
Connie <conniegiann@gmail.com>
2015-07-15 20:49:31 +1000

    chatter feed

---
+++ app/assets/javascripts/empty.js
@@ -0,0 +1,35 @@
+// $(document).ready( function () {
+
+//   var $userWebsite = $('.user-website')
+//   if ($userWebsite) {
+//     if ($userWebsite.val() === "") {
+//     $userWebsite.append(" www.website.com");
+//     }
+//   }
+
+//   var $userWebsite = $('.user-website')
+//   if ($userWebsite) {
+//     if ($userWebsite.val() === "") {
+//     $userWebsite.append(" www.website.com");
+//     }
+//   }
+
+//   var $userWebsite = $('.user-website')
+//   if ($userWebsite) {
+//     if ($userWebsite.val() === "") {
+//     $userWebsite.append(" www.website.com");
+//     }
+//   }
+
+//   var $userWebsite = $('.user-website')
+//   if ($userWebsite) {
+//     if ($userWebsite.val() === "") {
+//     $userWebsite.append(" www.website.com");
+//     }
+//   }    
+
+
+
+
+
+// });
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 # == Route Map
 #
-#      Prefix Verb   URI Pattern               Controller#Action
-#       posts GET    /posts(.:format)          posts#index
-#             POST   /posts(.:format)          posts#create
-#    new_post GET    /posts/new(.:format)      posts#new
-#   edit_post GET    /posts/:id/edit(.:format) posts#edit
-#        post GET    /posts/:id(.:format)      posts#show
-#             PATCH  /posts/:id(.:format)      posts#update
-#             PUT    /posts/:id(.:format)      posts#update
-#             DELETE /posts/:id(.:format)      posts#destroy
-#       users GET    /users(.:format)          users#index
-#             POST   /users(.:format)          users#create
-#    new_user GET    /users/new(.:format)      users#new
-#   edit_user GET    /users/:id/edit(.:format) users#edit
-#        user GET    /users/:id(.:format)      users#show
-#             PATCH  /users/:id(.:format)      users#update
-#             PUT    /users/:id(.:format)      users#update
-#             DELETE /users/:id(.:format)      users#destroy
-# session_new GET    /session/new(.:format)    session#new
-#        root GET    /                         posts#index
-#       login GET    /login(.:format)          session#new
-#             POST   /login(.:format)          session#create
-#             DELETE /login(.:format)          session#destroy
+#      Prefix Verb   URI Pattern                      Controller#Action
+#       posts GET    /posts(.:format)                 posts#index
+#             POST   /posts(.:format)                 posts#create
+#    new_post GET    /posts/new(.:format)             posts#new
+#   edit_post GET    /posts/:id/edit(.:format)        posts#edit
+#        post GET    /posts/:id(.:format)             posts#show
+#             PATCH  /posts/:id(.:format)             posts#update
+#             PUT    /posts/:id(.:format)             posts#update
+#             DELETE /posts/:id(.:format)             posts#destroy
+# user_tweets GET    /users/:user_id/tweets(.:format) users#tweets
+#       users GET    /users(.:format)                 users#index
+#             POST   /users(.:format)                 users#create
+#    new_user GET    /users/new(.:format)             users#new
+#   edit_user GET    /users/:id/edit(.:format)        users#edit
+#        user GET    /users/:id(.:format)             users#show
+#             PATCH  /users/:id(.:format)             users#update
+#             PUT    /users/:id(.:format)             users#update
+#             DELETE /users/:id(.:format)             users#destroy
+#        root GET    /                                session#new
+#             GET    /users/:id/tweets(.:format)      users#tweets
+#      signup GET    /signup(.:format)                users#new
+#       login GET    /login(.:format)                 session#new
+#             POST   /login(.:format)                 session#create
+#             DELETE /login(.:format)                 session#destroy
 #
 
 Rails.application.routes.draw do
