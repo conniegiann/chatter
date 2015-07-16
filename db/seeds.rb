f421ff1167293f36424fd625c07e4b6d16859923
Connie <conniegiann@gmail.com>
2015-07-16 15:07:07 +1000

    faker gem & seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Post.destroy_all
+
+20.times do
+  u = User.create :email => Faker::Internet.email, :username => Faker::Name.first_name, :password => "pasword99", :password_confirmation => "pasword99", :location => Faker::Address.city, :image => Faker::Avatar.image("90x190"), :bio => Faker::Company.catch_phrase, :website => Faker::Internet.domain_name
+
+  10.times do
+    p = Post.create :content => Faker::Hacker.say_something_smart
+    u.posts << p
+  end
+end
